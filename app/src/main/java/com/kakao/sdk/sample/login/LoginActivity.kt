4db5633a08ac5d075e161a114fb75e0819e678cf
Hara Kang <hara0115@gmail.com>
2019-06-21 16:03:00 +0900

    improve login code

--- app/src/main/java/com/kakao/sdk/sample/login/LoginActivity.kt
+++ app/src/main/java/com/kakao/sdk/sample/login/LoginActivity.kt
@@ -10,25 +10,39 @@ import com.kakao.sdk.sample.MainActivity
 import com.kakao.sdk.sample.R
 import com.kakao.sdk.sample.databinding.ActivityLoginBinding
 import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), CoroutineScope {
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
+    private lateinit var job: Job
+
     private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
-
+        job = Job()
         binding.kakaoLoginButton.setOnClickListener {
-            GlobalScope.launch {
-                val code = AuthCodeService.instance.requestAuthCode(this@LoginActivity)
-                withContext(Dispatchers.IO) {
-                    AuthApiClient.instance.issueAccessToken(authCode = code)
-                }
-                val mainIntent = Intent(this@LoginActivity, MainActivity::class.java)
-                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or
-                        Intent.FLAG_ACTIVITY_CLEAR_TASK or
-                        Intent.FLAG_ACTIVITY_CLEAR_TOP
-                startActivity(mainIntent)
-            }
+            loginButtonClicked()
         }
+    }
+
+    override fun onDestroy() {
+        job.cancel()
+        super.onDestroy()
+    }
+
+    private fun loginButtonClicked() = launch {
+        val code = AuthCodeService.instance.requestAuthCode(this@LoginActivity)
+        withContext(Dispatchers.IO) {
+            AuthApiClient.instance.issueAccessToken(authCode = code)
+        }
+        val mainIntent = Intent(this@LoginActivity, MainActivity::class.java)
+        mainIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or
+                Intent.FLAG_ACTIVITY_CLEAR_TASK or
+                Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(mainIntent)
     }
 }
